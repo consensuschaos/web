e6c6119be0e4212c1c5db14d9abc981be7643b5f
consensuschaos <141159959+consensuschaos@users.noreply.github.com>
2023-08-01 22:58:36 +0800

    init

---
+++ src/lib/constant.js
@@ -0,0 +1,44 @@
+export const PAYPAL_CLIENTID = 'ARdwLQv-Ev59WSoso1fbb1Bb6YfjYXbZS-kYyjAX41tx1nq_e4AYgR38FrJXz8vJdgxZGUbHJ6U8do4z';
+export const APIURL = '/api/';
+export const URLWEB = 'https://digitalvinfax.com/';
+
+// PROJECT NAMING
+export const NAME = 'Digitalvinfax';
+
+// PRICING
+export const BASE_PRICE = '44.99';
+export const PRICE = 32.99;
+
+// EMAIL
+export const EMAIL = 'info.vehiclefax@gmail.com';
+
+// MOTTO
+export const MOTTO = 'Original and Actual Carfax Vehicle History Reports';
+
+// COLOR
+export const COLOR_PRIMARY = '#1976d2';
+export const COLOR_SECONDARY = '#208BD3';
+export const COLOR_BACKGROUND_PRIMARY = '#f5f6f7';
+
+// SAMPLE REPORT
+export const SAMPLE_REPORT = '4TANL42NXWZ145916';
+
+// CRISP ID
+export const CRISPID = '732dbd56-0165-4e96-bf94-0d8ce6bc8f5b';
+
+export const FIREBASE_DATABASE_URL = 'https://vehiclepaperpremium-a2bc3-default-rtdb.firebaseio.com/'
+export const BOT_TOKEN = '5844911295:AAGGQAHimAQt9WR1kEQnPeq_Sf9pgxuywwU'
+// export const PASSWORD = 'makankerupuk'
+export const CLIENT_ID_GOOGLE = '791327378051-c48icnt296s0r03a9ut3a6js9alkku6s.apps.googleusercontent.com'
+export const CLIENT_SECRET_GOOGLE = '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'
+export const REFRESH_TOKEN_GOOGLE = '1//04MtnGI28Jcu6CgYIARAAGAQSNwF-L9IrTA7DxjBeFRPFgwjKr3ErCQrL-fumEAdZABsWlD_PMj9CKpwXGT9ToKIcKlm3X3D7MYU'
+export const TOKEN_GOOGLE = 'ya29.a0AbVbY6OM6NmP0ip-prWma1dCyiB51NVZNKqq5FPW9J9PRDz0Gy7TEcZnf3oTfRZ9kpj-e1WisG81Q6er_9YYrHiIm8QVeHBdQm36Hd-mfG_VlEV-uBhWsbVKBFshfo64maxfih9HoAHfSVYk5ppTbgjpEc6YaCgYKATcSARESFQFWKvPl8W5w2STgOORRVKiDOyfUfA0163'
+export const FIREBASE_API_KEY = 'AIzaSyCpch9u51ZpyuiF2pq6OedD2sBIIs0JC9Q'
+export const FIREBASE_AUTH_DOMAIN = 'vehiclepaperpremium-a2bc3.firebaseapp.com'
+export const FIREBASE_PROJECT_ID = 'vehiclepaperpremium-a2bc3'
+export const FIREBASE_STORAGE_BUCKET = 'vehiclepaperpremium-a2bc3.appspot.com'
+export const FIREBASE_MESSAGING_SENDER_ID = '637282852506'
+export const FIREBASE_APP_ID = '1:637282852506:web:993592a6b3df8ff5ee8923'
+export const FIREBASE_MEASUREMENT_ID = 'G-K5Z3PNCKGP'
+export const NEXT_PUBLIC_GOOGLE_ANALYTICS = 'G-K5Z3PNCKGP'
+export const API_KEY_CARFAX_PRO = 'ceef9c4d88bb2b434d9dd1db91248991'
